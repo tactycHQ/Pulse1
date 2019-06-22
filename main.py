b930173dca4720dd60fbe14fb45750eaf7a460c6
anubhav13@gmail.com <Evolution2019!>
2019-06-21 17:37:40 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
+#Pulse I
 import os
 import numpy as np
 os.environ['TF_KERAS'] = '1'
 from data import twitterProcessor
+import tensorflow as tf
 from model import BertModel
 from keras_bert import bert, gen_batch_inputs, get_base_dict, get_custom_objects
 from tensorflow.python.ops.math_ops import erf, sqrt
-from tensorflow.data import Dataset
+from utils.config import get_config_from_json
 
 
 def main():
+    # Processing config file
+    config = get_config_from_json('.//config.json')
+    load_flag = config.setup.load
+    data_dir = config.paths.data_dir
 
-    pretrained_path = 'uncased_L-12_H-768_A-12/'
-    config_path = pretrained_path + 'bert_config.json'
-    checkpoint_path = pretrained_path + 'bert_model.ckpt'
-    vocab_path = pretrained_path + 'vocab.txt'
-    data_dir = "data/train.csv"
-
-    SEQ_LEN = 128
-    BATCH_SIZE = 16
-    EPOCHS = 1
-    LR = 1e-4
-
-    processor = twitterProcessor(vocab_path, data_dir, SEQ_LEN)
+    processor = twitterProcessor(config.paths.vocab_path, data_dir, config.model.seq_len)
     x_train, y_train = processor.get_train_examples(data_dir)
+    num_samples = y_train.shape[0]
 
-    model = BertModel(config_path,
-                      checkpoint_path,
-                      SEQ_LEN,
-                      BATCH_SIZE,
-                      EPOCHS,
-                      LR)
-    model.load_pre_trained_model()
-    model = model.create_model(y_train.shape[0])
+    model = BertModel(config.paths.config_path,
+                      config.paths.ckpt_path,
+                      config.model.seq_len,
+                      config.model.batch_size,
+                      config.model.epochs,
+                      config.model.lr)
 
-    model.fit(x_train,
-              y_train,
-              epochs=EPOCHS,
-              batch_size=BATCH_SIZE
-              )
-    model.save('.//h5models//run1.h5')
+    if load_flag == False:
+        try:
+            model = model.finetune_model(num_samples, config.model.loss_fn,config.model.metrics)
+            model.fit(x_train,
+                      y_train,
+                      epochs=config.model.epochs,
+                      batch_size=config.model.batch_size
+                      )
+            model.save('.//h5models//'+config.setup.run_num)
+            print("Model saved succesfully as "+config.setup.run_num)
+        except Exception as ex:
+            print(ex)
+            print("Unable to create new fine tuned BERT model")
+    else:
+        try:
+            model.load(".\\h5models\\"+config.setup.run_num)
+            print("Model loaded succesfully from "+config.setup.run_num)
+            tf.saved_model.save(model.model,".\\served_models\\1",signatures=None)
+            print("Model converted to PB")
+        except Exception as ex:
+            print(ex)
+            print("Unable to load model")
 
+#Gelu function as per GitHub bug for TF2
 def gelu(x):
     return 0.5 * x * (1.0 + erf(x / sqrt(2.0)))
 
--- model.py
+++ model.py
@@ -1,14 +1,10 @@
 from tensorflow import keras
-from keras_bert import AdamWarmup, calc_train_steps
-from keras_bert import load_trained_model_from_checkpoint
-from keras_bert import backend
-
+from keras_bert import AdamWarmup, calc_train_steps, get_custom_objects, load_trained_model_from_checkpoint, backend
 
 
 class BertModel():
 
     def __init__(self,config_path, checkpoint_path,seq_len,batch_size,epochs, lr):
-        tf_keras = 1
         self.config_path = config_path
         self.checkpoint_path = checkpoint_path
         self.seq_len = seq_len
@@ -16,7 +12,7 @@ class BertModel():
         self.epochs = epochs
         self.lr = lr
         self.model = None
-
+        self.load_pre_trained_model()
 
     def load_pre_trained_model(self):
         self.pretrained_model = load_trained_model_from_checkpoint(
@@ -27,7 +23,7 @@ class BertModel():
                                                 seq_len = self.seq_len,
         )
 
-    def create_model(self,data_size):
+    def finetune_model(self,data_size,loss_fn,metrics):
         inputs = self.pretrained_model.inputs[:2]
         dense = self.pretrained_model.get_layer('NSP-Dense').output
         outputs = keras.layers.Dense(units=2, activation='softmax')(dense)
@@ -38,12 +34,22 @@ class BertModel():
         )
 
         model = keras.models.Model(inputs, outputs)
+
         model.compile(
             AdamWarmup(decay_steps=decay_steps, warmup_steps=warmup_steps, lr=self.lr),
-            loss='sparse_categorical_crossentropy',
-            metrics=['sparse_categorical_accuracy'],
+            loss=loss_fn,
+            metrics=[metrics],
         )
         self.model = model
         return self.model
+
+    def load(self, checkpoint_path):
+        """
+        loads an H5 file
+        :param checkpoint_path:file path
+        :return:
+        """
+        self.model = keras.models.load_model(checkpoint_path,custom_objects = get_custom_objects())
+
 
 
